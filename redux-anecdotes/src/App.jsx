d5fd6e65b2e51f727bd0990ca11b2ab9ee59e130
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-02-10 20:10:16 +0200

    6.7 cleanup

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { create, vote } from './reducers/anecdoteReducer'
+import { vote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
