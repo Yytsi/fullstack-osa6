fdc9cdab1e2e399af9f0d0b43cdb8c3cf55fe139
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-02-10 20:16:27 +0200

    6.8

---
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const voteForAnecdote = (id) => {
+    dispatch(vote(id))
+  }
+
+  return (
+    <div>
+      {[...anecdotes].sort((a,b) => b.votes - a.votes).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => voteForAnecdote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
